014fa1a1444e302c0e0332a47281d881a53bd5a5
Komecitos <irga.prayoga273@gmail.com>
2026-06-13 18:21:25 +0700

    fix: redirect root route to Dashboard module

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Dashboard\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
